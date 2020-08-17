ef27c5bc07e6159ef918eac4a32900dd5386a154
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-17 16:14:36 -0500

    added rows for each hour of the day

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 
-  var day = moment().format('LL');  
+  var day = moment().format('LL');
+  var currentTime = moment().format('LT');
+  var currentHour = moment().format('H');
+  var timeblocksContainer = $("#timeblocks");
+
   $("#currentDay").text(day);
+
+for (var i = 9; i <= 17 ; i++) {
+  var plannerRow = $("<div>");
+  $("#timeblocks").append("<div class=row></div>");
+
+}
+
 
 
 });
