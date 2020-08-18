67056e33075470792444df772bcab579af9300a4
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-18 15:48:23 -0500

    set up code to change background color of blocks dependant on the time

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   var currentHour = moment().format('H');
   var timeblocksContainer = $("#timeblocks");
 
+
   // displays current day at the top of the screen
   $("#currentDay").text(day);
 
@@ -18,7 +19,22 @@ $(document).ready(function(){
     $(newTimeBlock).addClass("time-block col-1");
     $(newDescription).addClass("description col-11");
 
-    if (hour < 12){
+    // checks what background color should be for each row
+    if (currentHour < hour) {
+      $(newRow).css("background-color", "green");
+    }
+    else if (currentHour == hour) {
+      $(newRow).css("background-color", "red");
+    }
+    else {
+      $(newRow).css("background-color", "grey");
+    }
+    
+    console.log(hour);
+    console.log(currentHour);
+  
+    // formats to am and pm
+    if (hour < 12) {
       $(newTimeBlock).text(hour + " am");
     }
     else if (hour === 12) {
@@ -28,11 +44,30 @@ $(document).ready(function(){
       $(newTimeBlock).text((hour - 12) + " pm");
     }
 
+    // creates space for event description
     $(newDescription).text("")
 
+
+    
+
+    // changes background color dependant on current hour
+    if (newRow.hour < currentHour) {
+      $(newRow).css("background-color", "black");
+    }
+
+    // append elements
     $(timeblocksContainer).append(newRow);
     $(newRow).append(newTimeBlock);
     $(newRow).append(newDescription);
   }
 
+
+
+  // need to add code to have modal pop up
+  $(".row").on("click", function(){
+    console.log("A row has been clicked");
+  });
 });
+
+
+  
