60ca64f978a8d0b7e2b08b8a19578e221094fada
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-18 14:34:56 -0500

    created a for loop to create rows from 9 - 5 with timeblocks

--- script.js
+++ script.js
@@ -5,14 +5,34 @@ $(document).ready(function(){
   var currentHour = moment().format('H');
   var timeblocksContainer = $("#timeblocks");
 
+  // displays current day at the top of the screen
   $("#currentDay").text(day);
 
-for (var i = 9; i <= 17 ; i++) {
-  var plannerRow = $("<div>");
-  $("#timeblocks").append("<div class=row></div>");
+  // creates blocks from 9 am to 5 pm
+  for (var hour = 9; hour <=17; hour++) {
+    var newRow = $("<div>");
+    var newTimeBlock = $("<p>");
+    var newDescription = $("<p>");
 
-}
+    $(newRow).addClass("row");
+    $(newTimeBlock).addClass("time-block col-1");
+    $(newDescription).addClass("description col-11");
 
+    if (hour < 12){
+      $(newTimeBlock).text(hour + " am");
+    }
+    else if (hour === 12) {
+      $(newTimeBlock).text(hour + " pm");
+    }
+    else {
+      $(newTimeBlock).text((hour - 12) + " pm");
+    }
 
+    $(newDescription).text("")
+
+    $(timeblocksContainer).append(newRow);
+    $(newRow).append(newTimeBlock);
+    $(newRow).append(newDescription);
+  }
 
 });
